969d9bfdda85fa5227a3e287196f0e1e561a5e38
tanjil <tanjil14@cse.pstu.ac.bd>
2021-08-18 09:38:49 +0600

    Complete some functionality

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('I am in a separate file')
+function updateProductNumber(product,price,isIncreasing){
+const productInput=document.getElementById(product +'-number')
+let productNumber=productInput.value;
+if(isIncreasing){
+    productNumber=parseInt(productNumber) +1;
+}else if(productNumber>0){
+    productNumber=parseInt(productNumber) -1;
+}
+productInput.value=productNumber;
+//update total
+const productTotal=document.getElementById(product +'-total');
+productTotal.innerText=productNumber * price;
+//update calculation
+calculate()
+
+}
+function getInputValue(product){
+    const productInput=document.getElementById(product+'-number');
+    const productNumber=parseInt(productInput.value);
+    return productNumber;
+}
+//calculation
+function calculate(){
+    const phoneTotal=getInputValue('phone')*1219;
+    const caseTotal=getInputValue('case')*59;
+    const subTotal=phoneTotal+caseTotal;
+    const tax=subTotal/10;
+    const total=subTotal+tax;
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-total').innerText=tax;
+    document.getElementById('total').innerText=total;
+}
+
+//increase decrease handler
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true)
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false)
+})
+document.getElementById('case-plus').addEventListener('click',function(){
+    updateProductNumber('case',59,true)
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateProductNumber('case',59,false)
+})
